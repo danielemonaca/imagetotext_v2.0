bc41805a840f9c5f7d3b1f77e001a1b0237d4f42
Daniele Monaca <danielemonaca@users.noreply.github.com>
2019-05-08 00:09:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# imagetotext_v2.0
---
+++ src/com/imagetotext/com/Main.java
@@ -0,0 +1,26 @@
+package com.imagetotext.com;
+import java.io.PrintWriter;
+public class Main {
+public static void main(String[] args) {
+ String input_file="C:\\Users\\Daniele\\Desktop\\live_quiz\\33.png"; 
+ String output_file="C:\\Users\\Daniele\\Desktop\\live_quiz\\33"; //No need to specify .txt
+ String tesseract_install_path="C:\\Program Files (x86)\\Tesseract-OCR\\tesseract"; //Tesseract path folder
+ String[] command =
+    {
+        "cmd",
+    };
+    Process p;
+ try {
+        p = Runtime.getRuntime().exec(command);
+        new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+        new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+        PrintWriter stdin = new PrintWriter(p.getOutputStream());
+        stdin.println("\""+tesseract_install_path+"\" \""+input_file+"\" \""+output_file+"\" -l eng");
+        stdin.close();
+        p.waitFor();
+        System.out.println("\n"+Read_File.read_a_file(output_file+".txt")); //Result
+    } catch (Exception e) {
+ e.printStackTrace();
+    }
+  }
+}
